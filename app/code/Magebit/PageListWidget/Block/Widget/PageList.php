7a41c50fbd89ec9988d6bb19f5235270b04de619
EdLiseksMB <edgars.liseks@magebit.com>
2022-11-09 07:28:19 +0200

    ML01 - Magebit Learning

---
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Magebit\PageListWidget\Block\Widget;
+
+use Magento\Cms\Api\PageRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+use Psr\Log\LoggerInterface;
+
+
+/**
+ * Block for list of links to selected CMS pages
+ */
+class PageList extends Template implements BlockInterface
+{
+    /**
+     * Display mode set to specific pages
+     */
+    const SPECIFIC_PAGES = 'specific_pages';
+
+    /**
+     * @var array
+     */
+    private array $data;
+
+    /**
+     * @var \Magento\Cms\Api\Data\PageInterface[]
+     */
+    protected array $pages = [];
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+
+    /**
+     * @var PageRepositoryInterface
+     */
+    private PageRepositoryInterface $pageRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
+     * @param Template\Context $context
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param PageRepositoryInterface $pageRepository
+     * @param LoggerInterface $logger
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        PageRepositoryInterface $pageRepository,
+        LoggerInterface $logger,
+        array $data = []
+    ) {
+        $this->pageRepository = $pageRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->logger = $logger;
+        $this->data = $data;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return void
+     */
+    protected function _construct(): void
+    {
+        $this->setTemplate('page_list.phtml');
+    }
+
+    /**
+     * Gets selected CMS pages
+     *
+     * @return array
+     */
+    public function getPages(): array
+    {
+        if (!empty($this->pages)) {
+            return $this->pages;
+        }
+
+        if ($this->data['display_mode'] === self::SPECIFIC_PAGES) {
+            $pageIdentifiers = explode(',', $this->data['selected_pages']);
+            $this->searchCriteriaBuilder->addFilter('page_id', $pageIdentifiers, 'in');
+        }
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        try {
+            $this->pages = $this->pageRepository->getList($searchCriteria)->getItems();
+        } catch (LocalizedException $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return $this->pages;
+    }
+}
